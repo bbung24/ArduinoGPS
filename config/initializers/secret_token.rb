852a163694496f6b662112d6534339897ea63b72
Inho Yong <iyong@andrew.cmu.edu>
2013-11-20 17:38:58 -0500

    first time commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ArduinoGPS::Application.config.secret_key_base = '83dbfce1332a453591fbd007babe90a7c28a410d2f9e699179744285708664c24835120e95a6fb2afaed199b83792e0abd72e2c09c52910b69057faa25a88bbf'
